d72d877f358eefd25d77c73c4ab9beb9cc334dfb
Tom Czarniecki <tomcz@users.noreply.github.com>
2018-09-07 02:08:37 -0400

    make zone_id a computed field

--- cloudflare/resource_cloudflare_access_rule.go
+++ cloudflare/resource_cloudflare_access_rule.go
@@ -29,7 +29,7 @@ func resourceCloudflareAccessRule() *schema.Resource {
 			"zone_id": {
 				Type:     schema.TypeString,
 				Optional: true,
-				ForceNew: true,
+				Computed: true,
 			},
 			"mode": {
 				Type:         schema.TypeString,
